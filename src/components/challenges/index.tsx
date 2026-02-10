fafea35f1af3ffab03b174a3f1b719b7ba8a967c
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-10 16:18:35 -0300

    Criando script para exibir desafios completos

---
+++ src/components/challenges/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+export function Challenges(){
+  
+  return(
+    <span className="challenges-container">
+      <span>Desafios completos</span>
+      <span>00</span>
+    </span>
+  )
+}
