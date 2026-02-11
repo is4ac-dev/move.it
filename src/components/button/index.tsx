29bcfe83918844a5e793623508c42728287d9222
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 10:03:19 -0300

    Adicionando condicionais baseadas no contexto do button para exibir seu estado

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,12 +1,27 @@
 import "./styles.css"
 
-import { BuildStartBtn, BuildCancelBtn } from "./build-btn"
+import { useContext, type ReactNode } from "react"
+import { PomoContext } from "../../contexts/PomoContext.ts"
+
+import { BuildStartBtn, BuildCancelBtn, BuildFinishedBtn } from "./build-btn"
 
 export function Button(){
 
+  const { isWorking, isFinished } = useContext(PomoContext)
+
+  let buttonContent: ReactNode
+
+  if (isFinished){
+    buttonContent = <BuildFinishedBtn/>
+  } else if(isWorking){
+    buttonContent = <BuildCancelBtn/>
+  } else{
+    buttonContent = <BuildStartBtn/>
+  }
+
   return(
     <div className="button-container">
-      { false ? <BuildCancelBtn/> : <BuildStartBtn/>}
+      {buttonContent}
     </div>
   )
 }
