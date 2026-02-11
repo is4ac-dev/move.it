d46b9fd9d0d30e7ddd509c7ff310c7b672d7d823
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 08:04:58 -0300

    Exibindo timer no seu estado atual

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,20 +1,12 @@
 import "./styles.css"
 
+import { BuildDefaultTimer, BuildFlexTimer } from "./buildTimer.tsx"
+
 export function Timer(){
 
+  const isWorking = false
+
   return (
-    <div className="timer-container">
-      <div className="number-box">
-        <span>2</span>
-        <div className="divisor"></div>
-        <span>5</span>
-      </div>
-      <div className="separator">:</div>
-      <div className="number-box">
-        <span>0</span>
-        <div className="divisor"></div>
-        <span>0</span>
-      </div>
-    </div>
+    isWorking ? <BuildFlexTimer/> : <BuildDefaultTimer/>
   )
 }
