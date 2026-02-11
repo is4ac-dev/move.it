1e50e0233ed92751b065e806f6caae68e4e3fdf8
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 19:01:44 -0300

    Adicionando reatividade para level do usuário

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,9 +1,13 @@
 import profileAvatar from "../../assets/avatar.jpg"
 import levelIcon from "../../assets/level.svg"
-
 import "./styles.css"
 
+import { UserDataContext } from "../../contexts/UserDataContext"
+import { useContext } from "react"
+
 export function Profile(){
+
+  const { level } = useContext(UserDataContext)
   
   return(
     <div className="profile-card">
@@ -12,7 +16,7 @@ export function Profile(){
         <span className="profile-name">Isaac Monteiro</span>
         <div className="profile-level">
           <img src={ levelIcon }/>
-          <span>Level 1</span>
+          <span>Level {level}</span>
         </div>
       </div>
     </div>
