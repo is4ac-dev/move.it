2d59d78cf349cade9f0aa10478ab7a96494472d3
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 12:02:27 -0300

    Ajustando chamada dos hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 import { ProgressBar } from "./components/progress/index.tsx"
 import { Card } from "./components/card/index.tsx"
 import { Profile } from "./components/profile/index.tsx"
@@ -7,28 +5,28 @@ import { Challenges } from "./components/challenges/index.tsx"
 import { Timer } from "./components/timer/index.tsx"
 import { Button } from "./components/button/index.tsx"
 
-import { PomoContext } from "./contexts/PomoContext.ts"
+import { PomoProvider } from "./contexts/PomoContext"
+import { UserDataProvider } from "./contexts/UserDataContext"
 
-export function App(){
+import "./styles/global.css"
 
-  const [isWorking, setIsWorking] = useState(false)
-
-  const [isFinished, setIsFinished] = useState(false)
-
-  return(
-    <PomoContext.Provider value={{isWorking, setIsWorking, isFinished, setIsFinished}}>
-      <div className="app-container">
-      <ProgressBar/>
-      <main>
-        <div className="client-section">
-          <Profile/>
-          <Challenges/>
-          <Timer/>
-          <Button/>
+export function App() {
+  return (
+    <UserDataProvider>
+      <PomoProvider>
+        <div className="app-container">
+          <ProgressBar />
+          <main>
+            <div className="client-section">
+              <Profile />
+              <Challenges />
+              <Timer />
+              <Button />
+            </div>
+            <Card />
+          </main>
         </div>
-        <Card/>
-      </main>
-      </div>
-    </PomoContext.Provider>
+      </PomoProvider>
+    </UserDataProvider>
   )
 }
