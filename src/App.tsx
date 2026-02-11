91ffe9161235a575eaa8e1c868bfc71b952466d6
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 10:02:08 -0300

    Adicionando contexto do pomodoro a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import { ProgressBar } from "./components/progress/index.tsx"
 import { Card } from "./components/card/index.tsx"
 import { Profile } from "./components/profile/index.tsx"
@@ -5,19 +7,28 @@ import { Challenges } from "./components/challenges/index.tsx"
 import { Timer } from "./components/timer/index.tsx"
 import { Button } from "./components/button/index.tsx"
 
+import { PomoContext } from "./contexts/PomoContext.ts"
+
 export function App(){
+
+  const [isWorking, setIsWorking] = useState(false)
+
+  const [isFinished, setIsFinished] = useState(false)
+
   return(
-    <div className="app-container">
-    <ProgressBar/>
-    <main>
-      <div className="client-section">
-        <Profile/>
-        <Challenges/>
-        <Timer/>
-        <Button/>
+    <PomoContext.Provider value={{isWorking, setIsWorking, isFinished, setIsFinished}}>
+      <div className="app-container">
+      <ProgressBar/>
+      <main>
+        <div className="client-section">
+          <Profile/>
+          <Challenges/>
+          <Timer/>
+          <Button/>
+        </div>
+        <Card/>
+      </main>
       </div>
-      <Card/>
-    </main>
-    </div>
+    </PomoContext.Provider>
   )
 }
