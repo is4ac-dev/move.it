7d96939fef9ec4e06ae2f4350a22e742808a7611
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-11 10:16:49 -0300

    Criando builder para timer zerado

--- src/components/timer/buildTimer.tsx
+++ src/components/timer/buildTimer.tsx
@@ -65,3 +65,21 @@ export function BuildFlexTimer(){
     </div>
   )
 }
+
+export function BuildZeroTimer(){
+  return (
+    <div className="timer-container">
+      <div className="number-box">
+        <span>0</span>
+        <div className="divisor"></div>
+        <span>0</span>
+      </div>
+      <div className="separator">:</div>
+      <div className="number-box">
+        <span>0</span>
+        <div className="divisor"></div>
+        <span>0</span>
+      </div>
+    </div>
+  )
+}
