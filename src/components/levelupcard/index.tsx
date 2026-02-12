b6ee8466afc06296651f2940dbbfae9997f34c53
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-12 10:01:16 -0300

    Atualizando componente de LevelUpCard para ser exibido somente ao subir de nível

--- src/components/levelupcard/index.tsx
+++ src/components/levelupcard/index.tsx
@@ -13,7 +13,25 @@ import "./styles.css"
 export function LevelUpCard() {
 
   // Recebendo contexto do User
-  const { level, closeLevelUpCard } = useContext(UserDataContext);
+  const { level, closeLevelUpCard, isLevelUpCardOpen } = useContext(UserDataContext);
+
+  // Se o estado do contexto for de não exibir, impedindo renderização
+  if(!isLevelUpCardOpen) return null
+
+  // Função para compartilhar progresso no X
+  function shareToX() {
+
+    // Utilizando essa função que codifica os dados da string para um formato que o navegador consiga ler
+    const shareText = encodeURIComponent(
+      `Acabei de subir para o nível ${level} no Move.it!`
+    );
+    
+    // Recebendo URL de compartilhamento do X
+    const xUrl = `https://x.com/intent/tweet?text=${shareText}`;
+    
+    // Abrindo uma nova aba de compartilhamento no navegador
+    window.open(xUrl, '_blank');
+  }
 
   // Retornando estrutura do contexto
   return (
@@ -26,7 +44,7 @@ export function LevelUpCard() {
           <img src={ cancelIcon }/>
         </button>
         <footer className="footer">
-          <button type="button" className="xButton">
+          <button type="button" className="xButton" onClick={shareToX}>
             Compartilhar no
             <img src={ xIcon }/>
           </button>
