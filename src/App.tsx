59969889abdb05dc5978676ab9c93d5c77dd7957
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-12 10:41:20 -0300

    Otimizando componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
 // Importando Componentes de Renderização
-import { ProgressBar } from "./components/progress/index.tsx"
-import { Card } from "./components/card/index.tsx"
-import { Profile } from "./components/profile/index.tsx"
-import { Challenges } from "./components/challenges/index.tsx"
-import { Timer } from "./components/timer/index.tsx"
-import { Button } from "./components/button/index.tsx"
-import { LevelUpCard } from "./components/levelupcard/index.tsx"
+import { MainPage } from "./components/indexMainPage.tsx"
+import { LoginPage } from "./components/login/index.tsx"
 
-// Importando Componentes de Contexto
-import { PomoProvider } from "./contexts/PomoContext"
-import { UserDataProvider } from "./contexts/UserDataContext"
+// Importando contexto do User
+import { useContext } from "react"
+import { UserDataContext } from "./contexts/UserDataContext.tsx"
 
 // Criando componente para renderização da aplicação
 export function App() {
 
-  // Retornando estrutura do componente
+  // Recebendo contexto do User
+  const { isLoggedIn } = useContext(UserDataContext)
+
+  // Retornando estrutura HTML
   return (
-    <UserDataProvider>
-      <PomoProvider>
-        <div className="app-container">
-          <ProgressBar />
-          <main>
-            <div className="client-section">
-              <Profile />
-              <Challenges />
-              <Timer />
-              <Button />
-            </div>
-            <Card />
-          </main>
-        </div>
-        <LevelUpCard/>
-      </PomoProvider>
-    </UserDataProvider>
+    <>
+      { isLoggedIn ? <MainPage/> : <LoginPage/> }
+    </>
   )
 }
